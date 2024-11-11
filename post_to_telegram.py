2cff1b60ffa611e0b94eb4083ea932f130676277
zrain03 <150784633+zrain03@users.noreply.github.com>
2024-11-12 01:26:15 +0800

    Update and rename post.py to post_to_telegram.py

---
+++ post_to_telegram.py
@@ -0,0 +1,20 @@
+from telethon import TelegramClient
+import asyncio
+import os
+
+# Set your message and group IDs
+MESSAGE = "اللّٰهُمَّ صَلِّ وَسَلِّمْ عَلٰى سَيِّدِنَا مُحَمَّد\nAllahumma salli wasallim 'ala Sayyidina Muhammad"
+GROUP_IDS = [-1002288720559, -1002200241778]
+
+async def send_message():
+    api_id = int(os.getenv('API_ID'))
+    api_hash = os.getenv('API_HASH')
+    session_string = os.getenv('SESSION_STRING')
+
+    async with TelegramClient(session_string, api_id, api_hash) as client:
+        for group_id in GROUP_IDS:
+            await client.send_message(group_id, MESSAGE)
+        print("Message sent to all groups successfully.")
+
+if __name__ == "__main__":
+    asyncio.run(send_message())
