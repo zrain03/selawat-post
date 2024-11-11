04dcadfee66f8b9be2087eced6c72f039ca9b1c7
zrain03 <150784633+zrain03@users.noreply.github.com>
2024-11-12 01:31:55 +0800

    Update post_to_telegram.py

--- post_to_telegram.py
+++ post_to_telegram.py
@@ -6,14 +6,13 @@ import os
 MESSAGE = "اللّٰهُمَّ صَلِّ وَسَلِّمْ عَلٰى سَيِّدِنَا مُحَمَّد\nAllahumma salli wasallim 'ala Sayyidina Muhammad"
 GROUP_IDS = [-1002288720559, -1002200241778]
 
-# Define a path for the session file
-SESSION_FILE = "./telegram_session"
-
 async def send_message():
     api_id = int(os.getenv('API_ID'))
     api_hash = os.getenv('API_HASH')
+    session_string = os.getenv('SESSION_STRING')
 
-    async with TelegramClient(SESSION_FILE, api_id, api_hash) as client:
+    # Authenticate using the session string
+    async with TelegramClient(StringSession(session_string), api_id, api_hash) as client:
         for group_id in GROUP_IDS:
             await client.send_message(group_id, MESSAGE)
         print("Message sent to all groups successfully.")
